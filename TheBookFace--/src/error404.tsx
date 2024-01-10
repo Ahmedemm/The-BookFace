9294bbf44033b789407977bdc200f7c6013678ac
Julie Dussaussois <j.dussaussois@gmail.com>
2024-01-10 12:05:32 +0100

    notif and error404 ans not found

---
+++ TheBookFace--/src/error404.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Error404: React.FC = () => {
+  return (
+    <div>
+      <h2>Error 404</h2>
+      <p>The resource you are looking for could not be found.</p>
+    </div>
+  );
+};
+
+export default Error404;
